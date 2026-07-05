28e642fabf6fc2aa2bf6e6445c6ee091cbecb87a
Reito Kojima <kojimaray@gmail.com>
2026-07-05 20:40:59 +0900

    Added Location Check feature, disable heatmap depending on location

--- frontend/src/measure_page.js
+++ frontend/src/measure_page.js
@@ -6,6 +6,28 @@ const GEO_OPTIONS = {
     maximumAge: 0
 }
 
+const BOUNDS = {
+    minLat: 139.424,
+    minLng: 35.384,
+    maxLat: 139.433,
+    maxLng: 35.393,
+}
+
+function boundCheck(lat, lng) {
+    return (
+        lat >= BOUNDS.minLat &&
+        lat <= BOUNDS.maxLat &&
+        lng >= BOUNDS.minLng &&
+        lng <= BOUNDS.maxLng
+    );
+}
+
+function setStatus(message, type = '') {
+    const el = document.getElementById('status');
+    el.textContent = message;
+    el.className = type;
+}
+
 function getLocation() {
     if (!navigator.geolocation) {
         document.getElementById('status').textContent = 'This browser does not support this feature.';
@@ -14,6 +36,7 @@ function getLocation() {
 
     document.getElementById('button_locate').disabled = true;
     document.getElementById('status').textContent = 'Locating…';
+    setStatus('Obtaining GPS Location...');
 
     navigator.geolocation.getCurrentPosition(success, errorResult, GEO_OPTIONS);
 }
@@ -27,7 +50,23 @@ function success(position) {
     document.getElementById('status').textContent   = 'Location acquired.';
     document.getElementById('button_locate').disabled = false;
 
-    placeMarker(latitude, longitude, accuracy);
+    if (boundCheck(latitude, longitude)) {
+        setStatus('Location acquired.', 'success');
+
+        document.getElementById('heatmap-link').style.display = 'inline-flex';
+        document.getElementById('bounds-status').textContent = 'You are in SFC Campus.';
+
+        localStorage.setItem('latitude', latitude);
+        localStorage.setItem('longitude', longitude);
+        localStorage.setItem('accuracy', accuracy);
+    }
+
+    else {
+        setStatus('Location acquired', 'success')
+
+        document.getElementById('heatmap-link').style.display = 'none';
+        document.getElementById('bounds-status').textContent = 'You are outside SFC Campus.';
+    }
 }
 
 function errorResult(error) {
@@ -37,6 +76,7 @@ function errorResult(error) {
         3: 'Request timed out, please try again.'
     }
 
+    setStatus(messages[error.code] || 'Unknown error.', 'error');
     document.getElementById('status').textContent    = messages[error.code] || 'Unknown error.';
     document.getElementById('button_locate').disabled = false;
 }
